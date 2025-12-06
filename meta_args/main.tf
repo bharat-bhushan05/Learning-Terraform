432ed1316d2ebe25443c7111d8ba1154d02561b4
devops-bharat05 <bharatbhushan05@outlook.com>
2025-12-06 19:15:29 +0530

    meta_args\main.tf updated

--- meta_args/main.tf
+++ meta_args/main.tf
@@ -5,6 +5,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.7.2"
     }
+    local = {
+      source = "hashicorp/local"
+      version = "2.6.1"
+    }
   }
 }
 
@@ -27,3 +31,23 @@ output "generated_password" {
 # Ask user for filename
 # Ask user for message
 # Create file dynamically using local_file
+
+
+resource "local_file" "user_file" {
+  filename = var.user_filename
+  content  = var.user_message
+}
+
+variable "user_filename" {
+  type        = string
+  description = "Enter the filename to create (e.g., output.txt)"
+}
+
+variable "user_message" {
+  type        = string
+  description = "Enter the message to write to the file"
+}   
+
+output "file" {
+  value = "File name is ${local_file.user_file.filename}: Content is ${local_file.user_file.content}"  
+}
